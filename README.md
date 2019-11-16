e44dcfef7846f74a5680f4d653d8211a2d9ffe35
Freewind <nowindlee@gmail.com>
2019-11-17 02:23:38 +0800

    typescript-react-select-scroll-to-option-demo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Select Scroll to Option Demo
+==============================================
+
+在Chrome中，对于一个multiple的select，当通过text input定位某一个option时，
+第一次匹配上的时候，它会自动滚动到选中的option，但是删除text input的内容，再次输入时，
+它就不会scroll了（在firefox中可以）。
+
+这个demo演示了如何在react中实现scroll
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,59 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+
+type Option = {
+  value: string,
+  label: string
+}
+
+const options: Option[] = [
+  {value: '111', label: '111'},
+  {value: '222', label: '222'},
+  {value: '333', label: '333'},
+  {value: '444', label: '444'},
+  {value: '555', label: '555'},
+  {value: '666', label: '666'},
+  {value: '777', label: '777'},
+  {value: '888', label: '888'},
+]
 
 export default function Hello() {
+  const [selectedValue, setSelectedValue] = useState<string []>([]);
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  useEffect(() => {
+    const select = selectRef.current;
+    if (select) {
+      const option = select.options[select.selectedIndex];
+      if (option) {
+        option.scrollIntoView();
+      }
+    }
+  }, [selectedValue]);
+
+  function searchKeyword(keyword: string) {
+    const found = keyword && options.find(it => it.label.startsWith(keyword))
+    if (found) {
+      setSelectedValue([found.value])
+    } else {
+      setSelectedValue([])
+    }
+  }
+
   return <div>
-    <h1>Hello React</h1>
+    <h1>Input Text and Scroll To Matched Option</h1>
+    <div>
+      <div>
+        <input type="text" onChange={event => searchKeyword(event.target.value)}/>
+        <span>{selectedValue}</span>
+      </div>
+      <div>
+        <select multiple value={selectedValue} size={5} ref={selectRef}>
+          {
+            options.map(({value, label}) =>
+              <option key={value} value={value}>{label}</option>)
+          }
+        </select>
+      </div>
+    </div>
   </div>
 };
